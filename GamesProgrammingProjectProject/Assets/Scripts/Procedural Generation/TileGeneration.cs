1c7bc6e0ab42c5bfe5dad134c918c83646fa2877
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-02-23 14:05:07 +0000

    Using height map to change colour and height of single plane

--- GamesProgrammingProjectProject/Assets/Scripts/Procedural Generation/TileGeneration.cs
+++ GamesProgrammingProjectProject/Assets/Scripts/Procedural Generation/TileGeneration.cs
@@ -20,6 +20,15 @@ public class TileGeneration : MonoBehaviour
 	[SerializeField]
 	private float mapScale;
 
+	[SerializeField]
+	private TerrainType[] terrainTypes;
+
+	[SerializeField]
+	private float heightMultiplier;
+
+	[SerializeField]
+	private AnimationCurve heightCurve;
+
 	void Start()
 	{
 		GenerateTile();
@@ -38,6 +47,10 @@ public class TileGeneration : MonoBehaviour
 		// generate a heightMap using noise
 		Texture2D tileTexture = BuildTexture(heightMap);
 		this.tileRenderer.material.mainTexture = tileTexture;
+
+		// update the tile mesh vertices according to the height map
+		UpdateMeshVertices(heightMap);
+
 	}
 
 	private Texture2D BuildTexture(float[,] heightMap)
@@ -54,7 +67,13 @@ public class TileGeneration : MonoBehaviour
 				int colourIndex = z * tileWidth + x;
 				float height = heightMap[z, x];
 				// assign as color a shade of grey proportional to the height value
-				colourMap[colourIndex] = Color.Lerp(Color.black, Color.white, height);
+
+				// choose a terrain type according to the height value
+				TerrainType terrainType = ChooseTerrainType(height);
+				// assign the color according to the terrain type
+				colourMap[colourIndex] = terrainType.colour;
+
+				//colourMap[colourIndex] = Color.Lerp(Color.black, Color.white, height);
 			}
 		}
 
@@ -66,4 +85,58 @@ public class TileGeneration : MonoBehaviour
 
 		return tileTexture;
 	}
+	
+	TerrainType ChooseTerrainType(float height)
+	{
+		// for each terrain type, check if the height is lower than the one for the terrain type
+		foreach (TerrainType terrainType in terrainTypes)
+		{
+			// return the first terrain type whose height is higher than the generated one
+			if (height < terrainType.height)
+			{
+				return terrainType;
+			}
+		}
+		return terrainTypes[terrainTypes.Length - 1];
+	}
+
+	private void UpdateMeshVertices(float[,] heightMap)
+	{
+		int tileDepth = heightMap.GetLength(0);
+		int tileWidth = heightMap.GetLength(1);
+
+		Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+
+		// iterate through all the heightMap coordinates, updating the vertex index
+		int vertexIndex = 0;
+		for (int zIndex = 0; zIndex < tileDepth; zIndex++)
+		{
+			for (int xIndex = 0; xIndex < tileWidth; xIndex++)
+			{
+				float height = heightMap[zIndex, xIndex];
+
+				Vector3 vertex = meshVertices[vertexIndex];
+				// change the vertex Y coordinate, proportional to the height value. The height value is evaluated by the heightCurve function, in order to correct it.
+				meshVertices[vertexIndex] = new Vector3(vertex.x, this.heightCurve.Evaluate(height) * this.heightMultiplier, vertex.z);
+
+				vertexIndex++;
+			}
+		}
+
+		// update the vertices in the mesh and update its properties
+		this.meshFilter.mesh.vertices = meshVertices;
+		this.meshFilter.mesh.RecalculateBounds();
+		this.meshFilter.mesh.RecalculateNormals();
+		// update the mesh collider
+		this.meshCollider.sharedMesh = this.meshFilter.mesh;
+	}
+
+}
+
+[System.Serializable]
+public class TerrainType
+{
+	public string name;
+	public float height;
+	public Color colour;
 }
