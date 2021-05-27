830e59d864c514b3d44a0401b6d06412c61b2a88
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-05-27 18:23:22 +0100

    Play Button functionality

--- GamesProgrammingProjectProject/Assets/Scripts/MainMenu.cs
+++ GamesProgrammingProjectProject/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     
     public void PlayGame()
 	{
-		SceneManager.LoadScene("GameScene");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
 }
