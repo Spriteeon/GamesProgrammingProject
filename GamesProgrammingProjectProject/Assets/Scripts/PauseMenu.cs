0029efd2f8e1e9bc714d4ab6b4bc9c1bb706317b
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-05-28 21:06:18 +0100

    Pause Menu

---
+++ GamesProgrammingProjectProject/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+		{
+            if(isPaused)
+			{
+                Resume();
+			} else
+			{
+                Pause();
+			}
+		}
+    }
+
+    public void Resume()
+	{
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+	{
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+	}
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene("MenuScene");
+    }
+
+}
