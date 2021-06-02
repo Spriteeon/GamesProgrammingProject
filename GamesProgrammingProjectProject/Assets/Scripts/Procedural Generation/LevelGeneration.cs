1a66fbd1151e924eaf5f89b88a2b77074899caca
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-06-02 19:43:01 +0100

    Update LevelGeneration.cs

--- GamesProgrammingProjectProject/Assets/Scripts/Procedural Generation/LevelGeneration.cs
+++ GamesProgrammingProjectProject/Assets/Scripts/Procedural Generation/LevelGeneration.cs
@@ -112,7 +112,7 @@ public class LevelGeneration : MonoBehaviour
 
 	private float GenerateFreqAmp()
 	{
-		float freqAmp = Random.Range(0f, 4f);
+		float freqAmp = Random.Range(0f, 3f);
 		return freqAmp;
 	}
 
