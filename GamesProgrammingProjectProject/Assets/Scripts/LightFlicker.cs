7cd07d7006bb54e33dd880f7932329e5fa2aebee
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-06-03 21:20:48 +0100

    Adding Flickering Light

---
+++ GamesProgrammingProjectProject/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isFlickering)
+		{
+            StartCoroutine(FlickeringLight());
+		}
+    }
+
+    IEnumerator FlickeringLight()
+	{
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
